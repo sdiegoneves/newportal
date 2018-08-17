f8396f5e152b10c3d17595c43b872bc5ca788ea6
root <root@IM1DEVE016.iMusica.corp>
2018-08-17 17:36:51 -0400

    connection

--- app/routers/noticias.js
+++ app/routers/noticias.js
@@ -1,5 +1,18 @@
 module.exports = function(app){
 	app.get('/noticias', function(req, res){
-		res.render("noticias/noticia");
+
+		var mysql = require('mysql');
+
+		var connection = mysql.createConnection({
+			host: 'localhost',
+			user: 'root',
+			password: '12345',
+			database: 'portal_noticias'
+		});
+
+		connection.query("select * from noticias", function(error, result){
+			res.send(result);
+		});
+		
 	});
 };
