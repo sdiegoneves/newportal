10af0de8e0899a7199070b7a7868749203ce6bde
diego santana <ext-diego.santana@imusica.com.br>
2018-08-20 14:11:14 -0500

    OO no nodejs

--- config/server.js
+++ config/server.js
@@ -1,14 +1,18 @@
 var express = require('express');
+var consign = require('consign');
+var bodyParser = require('body-parser');
 
-var consign = require('consign')
+
 var app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
+app.use(bodyParser.urlencoded({extended:true}));
+
 consign().include('app/routers')
-.then('config/dbConnection.js')
-.then('app/models')
-.into(app);
+	.then('config/dbConnection.js')
+	.then('app/models')
+	.into(app);
 
 module.exports = app;
